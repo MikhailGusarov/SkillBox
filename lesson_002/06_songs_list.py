0ac3bb1bbf81bcf2ab84e589fe66ec2f34a73607
MikhailGusarov <vgg639@gmail.com>
2020-04-23 22:40:36 +0300

    Задача 06. Вычислил сколько звучат три другие песни в сумме. Задача завершена

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -39,4 +39,10 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+sum_3_songs = round(violator_songs_dict['Sweetest Perfection']
+                    + violator_songs_dict['Policy of Truth']
+                    + violator_songs_dict['Blue Dress'], 0)
+
+print('А другие три песни звучат {} минут'.format(sum_3_songs))
+
+
