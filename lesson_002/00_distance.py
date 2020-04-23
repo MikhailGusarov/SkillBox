81dddccb1918a6149a756926e261d44c540b3b28
MikhailGusarov <vgg639@gmail.com>
2020-04-23 21:44:53 +0300

    Выполнил первую задачу

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pprint
+
 # Есть словарь координат городов
 
 sites = {
@@ -12,11 +14,24 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
-
-# TODO здесь заполнение словаря
-
-print(distances)
+distances = {
+    'Moscow': {
+        'London':
+            ((sites['Moscow'][0] - sites['London'][0]) ** 2 + ((sites['Moscow'][1] - sites['London'][1]) ** 2)) ** 0.5,
+        'Paris':
+            ((sites['Moscow'][0] - sites['Paris'][0]) ** 2 + ((sites['Moscow'][1] - sites['Paris'][1]) ** 2)) ** 0.5},
+    'London': {
+        'Moscow':
+            ((sites['London'][0] - sites['Moscow'][0]) ** 2 + ((sites['London'][1] - sites['Moscow'][1]) ** 2)) ** 0.5,
+        'Paris':
+            ((sites['London'][0] - sites['Paris'][0]) ** 2 + ((sites['London'][1] - sites['Paris'][1]) ** 2)) ** 0.5},
+    'Paris': {
+        'Moscow':
+            ((sites['Paris'][0] - sites['Moscow'][0]) ** 2 + ((sites['Paris'][1] - sites['Moscow'][1]) ** 2)) ** 0.5,
+        'London':
+            ((sites['Paris'][0] - sites['London'][0]) ** 2 + ((sites['Paris'][1] - sites['London'][1]) ** 2)) ** 0.5}
+}
+pprint.pprint(distances)
 
 
 
