1ac25d527bc83288e7c770569ef55b0c4f09ff5f
MikhailGusarov <vgg639@gmail.com>
2020-04-26 13:56:05 +0300

    00. добавил в функцию изменение цвета и вывел 300 пузырков разного цвета и размера

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -3,23 +3,23 @@
 import simple_draw as sd
 
 
-def draw_bubble(center, radius, step):
+def draw_bubble(center, radius, step, color):
     """Function draw bubble - 3 circles with 1 center, different radius."""
     radius -= step
     for _ in range(3):
         radius += step
-        sd.circle(center, radius)
+        sd.circle(center, radius, color)
 
 
 sd.resolution = (1200, 600)
 
-# Нарисовать три ряда по 10 пузырьков
-for x in range(100, 1001, 100):
-    for y in range(200, 401, 100):
-        draw_bubble(sd.get_point(x, y), 50, 5)
-
-# Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+for _ in range(300):
+    x = sd.random_number(-100, 1300)
+    y = sd.random_number(-100, 700)
+    radius = sd.random_number(10, 50)
+    step = sd.random_number(3, 15)
+    color = sd.random_color()
+    draw_bubble(sd.get_point(x, y), radius, step, color)
 
 sd.pause()
 
