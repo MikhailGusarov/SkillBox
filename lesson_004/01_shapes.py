3f2b5a9876eafbe67897ce0272177b04fee9e933
MikhailGusarov <vgg639@gmail.com>
2020-04-29 18:45:51 +0300

    Модуль 4. 01 сделал отдельную функцию для рисования любой равносторонней фигуры. Переписал код других функций

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -28,70 +28,67 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
 
-def triangle(start_point, angle, length):
+def draw_equilateral_figure(start_point, angle, length, count_angles):
     """Draw triangle.
     Start_point is start position for draw (down left angle)
     angle is tilt angle base regarding axis x
     length is length one side"""
     new_point = start_point
-    for step_angle in range(angle, angle + 241, 120):
+    for step_angle in range(angle, angle + int((360 - 360 / count_angles)) + 1, int(360 / count_angles)):
         vector = sd.get_vector(start_point=new_point, angle=step_angle, length=length)
         vector.draw(width=3)
         new_point = vector.end_point
 
 
-def square(start_point, angle, length):
+def draw_triangle(start_point, angle, length):
+    """Draw triangle.
+    Start_point is start position for draw (down left angle)
+    angle is tilt angle base regarding axis x
+    length is length one side"""
+    draw_equilateral_figure(start_point=start_point, angle=angle, length=length, count_angles=3)
+
+
+def draw_square(start_point, angle, length):
     """Draw square.
     Start_point is start position for draw (down left angle)
     angle is tilt angle base regarding axis x
     length is length one side"""
-    new_point = start_point
-    for step_angle in range(angle, angle + 271, 90):
-        vector = sd.get_vector(start_point=new_point, angle=step_angle, length=length)
-        vector.draw(width=3)
-        new_point = vector.end_point
+    draw_equilateral_figure(start_point=start_point, angle=angle, length=length, count_angles=4)
 
 
-def pentagon(start_point, angle, length):
+def draw_pentagon(start_point, angle, length):
     """Draw triangle.
     Start_point is start position for draw (down left angle)
     angle is tilt angle base regarding axis x
     length is length one side"""
-    new_point = start_point
-    for step_angle in range(angle, angle + 289, 72):
-        vector = sd.get_vector(start_point=new_point, angle=step_angle, length=length)
-        vector.draw(width=3)
-        new_point = vector.end_point
+    draw_equilateral_figure(start_point=start_point, angle=angle, length=length, count_angles=5)
 
 
-def hexagon(start_point, angle, length):
+def draw_hexagon(start_point, angle, length):
     """Draw triangle.
     Start_point is start position for draw (down left angle)
     angle is tilt angle base regarding axis x
     length is length one side"""
-    new_point = start_point
-    for step_angle in range(angle, angle + 301, 60):
-        vector = sd.get_vector(start_point=new_point, angle=step_angle, length=length)
-        vector.draw(width=3)
-        new_point = vector.end_point
+    draw_equilateral_figure(start_point=start_point, angle=angle, length=length, count_angles=6)
 
 
 point = sd.get_point(100, 100)
-triangle(start_point=point, angle=10, length=100)
+draw_triangle(start_point=point, angle=10, length=100)
 
 point = sd.get_point(300, 100)
-square(start_point=point, angle=20, length=100)
+draw_square(start_point=point, angle=20, length=100)
 
 point = sd.get_point(100, 300)
-pentagon(start_point=point, angle=0, length=100)
+draw_pentagon(start_point=point, angle=0, length=100)
 
 point = sd.get_point(300, 300)
-hexagon(start_point=point, angle=0, length=100)
+draw_hexagon(start_point=point, angle=0, length=100)
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
 # Скажем, связывать точки не линиями, а дугами. Или двойными линиями. Или рисовать круги в угловых точках. Или...
 # А если таких функций не 4, а 44?
+# Прикинул,  многовато выходит. Но вроде можно
 
 # Часть 2 (делается после зачета первой части)
 #
