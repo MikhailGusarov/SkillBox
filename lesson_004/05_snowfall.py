83af502d4f312fabf6c4b0678068e3ac49478333
MikhailGusarov <vgg639@gmail.com>
2020-04-29 23:55:16 +0300

    Модуль 4. 05 готово

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -8,7 +8,8 @@ import simple_draw as sd
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+N = 50
+speed = 10
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,12 +18,46 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+
+def generate_snowflakes_data():
+    x = sd.random_number(0, sd.resolution[0])
+    y = sd.random_number(sd.resolution[1] - 50, sd.resolution[1] + 500)
+    length = sd.random_number(10, 40)
+    factor_a = sd.random_number(50, 70) / 100
+    factor_b = sd.random_number(25, 45) / 100
+    factor_c = sd.random_number(50, 70)
+    return {'x': x,
+            'y': y,
+            'length': length,
+            'factor_a': factor_a,
+            'factor_b': factor_b,
+            'factor_c': factor_c}
+
+
+snowflakes = []
+for _ in range(N):
+    snowflakes.append(generate_snowflakes_data())
+
 while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
+    for snowflake in snowflakes:
+        point = sd.get_point(snowflake['x'], snowflake['y'])
+        sd.snowflake(center=point,
+                     length=snowflake['length'],
+                     color=sd.background_color,
+                     factor_a=snowflake['factor_a'],
+                     factor_b=snowflake['factor_b'],
+                     factor_c=snowflake['factor_c'])
+        snowflake['y'] -= speed
+        snowflake['x'] += sd.random_number(-2, 2)
+        point = sd.get_point(snowflake['x'], snowflake['y'])
+        sd.snowflake(center=point,
+                     length=snowflake['length'],
+                     factor_a=snowflake['factor_a'],
+                     factor_b=snowflake['factor_b'],
+                     factor_c=snowflake['factor_c'])
+        if snowflake['y'] < 10:
+            snowflake['y'] = sd.random_number(sd.resolution[1] - 50, sd.resolution[1] + 300)
+    sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
