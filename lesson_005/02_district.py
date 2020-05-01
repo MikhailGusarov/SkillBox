5e7657d3eb1d1e626aa1484597e3ccd013d315e8
MikhailGusarov <vgg639@gmail.com>
2020-05-02 01:46:54 +0300

    Модуль 5 02 вывел всех бравых ребят с районаа

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -8,5 +8,25 @@
 # TODO здесь ваш код
 
 
+from district.central_street.house1 import room1 as cs_house1_room1
+from district.central_street.house1 import room2 as cs_house1_room2
+from district.central_street.house2 import room1 as cs_house2_room1
+from district.central_street.house2 import room2 as cs_house2_room2
+from district.soviet_street.house1 import room1 as ss_house1_room1
+from district.soviet_street.house1 import room2 as ss_house1_room2
+from district.soviet_street.house2 import room1 as ss_house2_room1
+from district.soviet_street.house2 import room2 as ss_house2_room2
+
+folks = []
+
+folks += (cs_house1_room1.folks)
+folks += (cs_house2_room1.folks)
+folks += (cs_house1_room2.folks)
+folks += (cs_house2_room2.folks)
+folks += (ss_house1_room1.folks)
+folks += (ss_house2_room1.folks)
+folks += (ss_house1_room2.folks)
+folks += (ss_house2_room2.folks)
+print('На районе живут', ', '.join(folks))
 
 
