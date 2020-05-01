ce267bd9fbd22132b9d61e8cc04248f9ee78a01a
MikhailGusarov <vgg639@gmail.com>
2020-05-02 01:51:00 +0300

    Модуль 5 01 fix.  некоректно понял задачу. Готов

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,29 +3,8 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-from district.central_street.house1 import room1 as cs_house1_room1
-from district.central_street.house1 import room2 as cs_house1_room2
-from district.central_street.house2 import room1 as cs_house2_room1
-from district.central_street.house2 import room2 as cs_house2_room2
-from district.soviet_street.house1 import room1 as ss_house1_room1
-from district.soviet_street.house1 import room2 as ss_house1_room2
-from district.soviet_street.house2 import room1 as ss_house2_room1
-from district.soviet_street.house2 import room2 as ss_house2_room2
+import room_1
+import room_2
 
-
-room_1 = []
-
-room_1 += (cs_house1_room1.folks)
-room_1 += (cs_house2_room1.folks)
-room_1 += (ss_house1_room1.folks)
-room_1 += (ss_house2_room1.folks)
-
-print('В комнате room_1 живут:', ', '.join(room_1))
-
-room_2 = []
-room_2 += (cs_house1_room2.folks)
-room_2 += (cs_house2_room2.folks)
-room_2 += (ss_house1_room2.folks)
-room_2 += (ss_house2_room2.folks)
-
-print('В комнате room_2 живут:', ', '.join(room_2))
+print('В комнате {} живут: {}'.format(room_1.__name__, ', '.join(room_1.folks)))
+print('В комнате {} живут: {}'.format(room_2.__name__, ', '.join(room_2.folks)))
