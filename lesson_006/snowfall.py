df2f8ae2e1e901a2a174e70a19dc659f6079ff97
MikhailGusarov <vgg639@gmail.com>
2020-05-07 15:14:05 +0300

    Модуль 6 02 заготовка модуля

---
+++ lesson_006/snowfall.py
@@ -0,0 +1,29 @@
+#  создать_снежинки(N) - создает N снежинок
+#  нарисовать_снежинки_цветом(color) - отрисовывает все снежинки цветом color
+#  сдвинуть_снежинки() - сдвигает снежинки на один шаг
+#  номера_достигших_низа_экрана() - выдает список номеров снежинок, которые вышли за границу экрана
+#  удалить_снежинки(номера) - удаляет снежинки с номерами из списка
+
+import simple_draw as sd
+
+_snowflakes = []
+
+
+def create_snowflakes(count_snowflakes):
+    pass
+
+
+def draw_snowflakes(color=sd.COLOR_WHITE):
+    pass
+
+
+def move_down():
+    pass
+
+
+def get_reached_bottom():
+    pass
+
+
+def delete_snowflakes(snowflakes):
+    pass
