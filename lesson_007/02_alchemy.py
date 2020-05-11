09fd74e67c977b5a77cc2f73721dbd6724ac0f00
MikhailGusarov <vgg639@gmail.com>
2020-05-11 23:52:26 +0300

    Модуль 7 02 Алхимия, часть 1

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,8 +19,123 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
 
+class Water:
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Water()
+        elif isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Soil):
+            return Dirt()
+
+
+class Air:
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+        elif isinstance(other, Air):
+            return Air()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Soil):
+            return Dust
+
+
+class Fire:
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Soil):
+            return Lava()
+        elif isinstance(other, Fire):
+            return Fire()
+
+
+class Soil:
+    def __str__(self):
+        return 'Земля'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Lava()
+        elif isinstance(other, Soil):
+            return Soil
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+
+    def __add__(self, other):
+        if isinstance(other, Storm):
+            return Storm
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+    def __add__(self, other):
+        if isinstance(other, Steam):
+            return Steam()
+
+
+class Dirt:
+    def __str__(self):
+        return 'Грязь'
+
+    def __add__(self, other):
+        if isinstance(other, Dirt):
+            return Dirt()
+
+
+class Lightning:
+    def __str__(self):
+        return 'Молния'
+
+    def __add__(self, other):
+        if isinstance(other, Lightning):
+            return Lightning
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+    def __add__(self, other):
+        if isinstance(other, Dust):
+            return Dust
+
+
+class Lava:
+    def __str__(self):
+        return 'Лава'
+
+    def __add__(self, other):
+        if isinstance(other, Lava):
+            return Lava
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Fire(), '+', Air(), '=', Fire() + Air())
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
 # Придумать что будет при сложении существующих элементов с новым.
