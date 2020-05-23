5a13a4fd123f2f20cd8557215d1990efd7c3c35a
MikhailGusarov <vgg639@gmail.com>
2020-05-23 23:49:03 +0300

    модуль 10 01 готово

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -4,17 +4,26 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+def generate_multi_pass_number():
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+
+try:
+    generate_multi_pass_number()
+except ValueError as ex:
+    print('пятый элемеент не является числом! Ошибка:', ex)
+except IndexError as ex:
+    print('вы ввели меньше 5 символов! Ошибка:', ex)
+except Exception as ex:
+    print('Неизвестная ошибка:', ex)
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
 
