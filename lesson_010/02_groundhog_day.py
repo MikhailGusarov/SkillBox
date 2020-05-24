c36118a59c8c1283ec84d2f582ac85b488c811fe
MikhailGusarov <vgg639@gmail.com>
2020-05-24 15:27:06 +0300

    модуль 10 02 первая версия дня сурка

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,65 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+from random import randint
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
 
-# https://goo.gl/JnsDqu
+class IamGodError(BaseException):
+    pass
+
+
+class DrunkError(BaseException):
+    pass
+
+
+class CarCrashError(BaseException):
+    pass
+
+
+class GluttonyError(BaseException):
+    pass
+
+
+class DepressionError(BaseException):
+    pass
+
+
+class SuicideError(BaseException):
+    pass
+
+
+def one_day():
+    errors = [IamGodError('Я - Бог'), DrunkError('Напился'), CarCrashError('Разбился на машине'),
+              GluttonyError('Объелся'), DepressionError('Умер от депрессии'), SuicideError('Самоубийство')]
+
+    rand_error = randint(0, 12)
+
+    if rand_error >= len(errors):
+        return randint(1, 7)
+    else:
+        raise errors[rand_error]
+
+
+karma = 0
+
+while ENLIGHTENMENT_CARMA_LEVEL > karma:
+    try:
+        karma += one_day()
+    except IamGodError as exc:
+        print(exc)
+    except DrunkError as exc:
+        print(exc)
+    except CarCrashError as exc:
+        print(exc)
+    except GluttonyError as exc:
+        print(exc)
+    except DepressionError as exc:
+        print(exc)
+    except SuicideError as exc:
+        print(exc)
+    else:
+        print('Текущее значение кармы:', karma)
+
+
